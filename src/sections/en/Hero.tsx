a31db48f1ee076cf7e31803cd8504dd7056211d9
kiyameh <kiyameh@outlook.com>
2025-06-11 01:09:12 +0200

    Incluido logo definitivo

--- src/sections/en/Hero.tsx
+++ src/sections/en/Hero.tsx
@@ -43,12 +43,34 @@ export default function Hero() {
         textAlign: 'center',
       }}
     >
+      <svg
+        style={{
+          maxHeight: '100px',
+          opacity: '70%',
+          padding: '1rem',
+        }}
+        width="66"
+        height="100"
+        viewBox="0 0 66 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0 4.99999C0 2.23857 2.23858 0 5 0H13.56C16.3214 0 18.56 2.23858 18.56 5V94.84C18.56 97.6014 16.3214 99.84 13.56 99.84H5C2.23858 99.84 0 97.6014 0 94.84V4.99999ZM36.569 44.2073C38.4637 42.38 41.4507 42.3345 43.4001 44.1035L47.2755 47.6202C49.3289 49.4836 49.4738 52.6621 47.5984 54.7046L40.794 62.1151C39.1228 63.9352 39.0317 66.7031 40.5797 68.6291L59.1281 91.7077C61.7578 94.9797 59.4286 99.84 55.2308 99.84H45.2556C43.7524 99.84 42.3289 99.1637 41.3794 97.9984L17.6248 68.8449C15.9827 66.8297 16.1588 63.8923 18.0299 62.0877L36.569 44.2073Z"
+          fill="var(--content)"
+        />
+        <circle
+          cx="53"
+          cy="37"
+          r="8"
+          fill="#FF197C"
+        />
+      </svg>
       {/* Title */}
       <h1 className="big-paragraph">
         I'm Andoni, and I'm a{' '}
         <span className="emphasis fantasy-text">software developer</span>
       </h1>
-
       {/* Subtitle */}
       <div
         className="big-paragraph"
