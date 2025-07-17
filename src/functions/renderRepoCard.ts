0ce68a906fc3c47563ded3ad2b32245d3ec05374
Kiyameh <kiyameh@outlook.com>
2025-07-17 16:46:43 +0200

    Corregida importación

--- src/functions/renderRepoCard.ts
+++
@@ -1,5 +0,0 @@
-import { renderRepoCard, type RepoStats } from "github-repo-stats-card";
-
-export function renderStatsCard(selector: string, stats: RepoStats) {
-  renderRepoCard(selector, stats);
-}
