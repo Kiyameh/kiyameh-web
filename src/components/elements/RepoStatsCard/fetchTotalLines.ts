caa3143f30a2dee97174cf528f85a4ff1f164b92
Kiyameh <kiyameh@outlook.com>
2025-07-18 21:06:07 +0200

    Segunda prueba de SSR

---
+++ src/components/elements/RepoStatsCard/fetchTotalLines.ts
@@ -0,0 +1,36 @@
+interface CodeTabsStats {
+  language: string;
+  linesOfCode: number;
+  bytes: number;
+}
+
+type CodeTabsApiResponse = CodeTabsStats[];
+
+export async function fetchTotalLines(
+  repository: string
+): Promise<number | undefined> {
+  try {
+    const author = repository.split("/")[3];
+    const slug = repository.split("/")[4];
+
+    const response = await fetch(
+      `https://api.codetabs.com/v1/loc?github=${author}/${slug}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data: CodeTabsApiResponse = await response.json();
+
+    const totalLines = data.find(
+      (item) => item.language === "Total"
+    )?.linesOfCode;
+
+    return totalLines;
+    
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
